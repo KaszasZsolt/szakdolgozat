eda7f4a42622eae7b76f430f807c94006e618b8d
KaszasZsolt <122326125+KaszasZsolt@users.noreply.github.com>
2025-03-13 21:06:31 +0100

    Update Layout.tsx
    fix: src/App.tsx(13,8): error TS2739: Type '{}' is missing the following properties from type 'NavbarProps': previewConfig, generatedCode, gameId, setGameId

--- megvalositas/client/src/components/baseUi/Layout.tsx
+++ megvalositas/client/src/components/baseUi/Layout.tsx
@@ -1,11 +1,19 @@
 import Navbar from "./Navbar";
-
+import { useState } from "react";
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
+  const defaultPreviewConfig = null; // or your desired default config
+  const defaultGeneratedCode = "";
+  const [gameId, setGameId] = useState<string | null>(null);
   return (
     <div className="flex flex-col">
-      <Navbar />
+     <Navbar
+        previewConfig={defaultPreviewConfig}
+        generatedCode={defaultGeneratedCode}
+        gameId={gameId}
+        setGameId={setGameId}
+      />
       <main className="flex-1">{children}</main>
     </div>
   );
